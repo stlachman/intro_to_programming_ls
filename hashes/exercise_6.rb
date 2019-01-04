7b1b02b1823b180672395a515358a1f520e2c999
Seth Lachman <lachmanseth17@gmail.com>
2019-01-03 22:20:43 -0500

    Completed exercise for hash chapter

--- hashes/exercise_6.rb
+++ hashes/exercise_6.rb
@@ -2,16 +2,19 @@ words = ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
 'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
 'flow', 'neon']
 
-def anagram(arr)
-  dictionary = {}
-  arr.each do |word|
-     if dictionary.has_key?(word) 
-      puts "That key exists"   
-     else
-        dictionary[word] = word
-     end
-  end
-  puts dictionary
-end
+  matches = {}
 
-anagram(words)
+  words.each do |word|
+    new_word = word.split("").sort.join
+    if matches.has_key?(new_word)
+      matches[new_word] << word
+    else
+      matches[new_word] = [word]
+    end
+  end
+
+  matches.each_value do |v|
+    p v
+  end
+
+
---
+++ hashes/exercise_7.rb
@@ -0,0 +1,6 @@
+x = "hi there"
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+
+# my_hash creates a hash with a symbol x as a key and "some value" as the value of the key x.
+# my_hash2 uses the variable x, in this case a string, to be the key for the value "some value".
